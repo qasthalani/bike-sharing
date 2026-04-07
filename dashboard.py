ee8871a4c29179e97d098616c3eb352c9c50f77b
Yasmin Qasthalani <qasthalaani@gmail.com>
2026-04-08 06:38:46 +0700

    Update dashboard.py
    update fontsize lagi

--- dashboard.py
+++ dashboard.py
@@ -127,10 +127,10 @@ sns.barplot(x=labels_pl,
             ax=ax1)
 
 ax1.set_title('Total Customers (Casual vs Registered)', loc='center', fontsize=30)
-ax1.set_xlabel('None')
-ax1.set_ylabel('None')
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
+ax1.set_xlabel(None)
+ax1.set_ylabel(None)
+ax1.tick_params(axis='y', labelsize=20)
+ax1.tick_params(axis='x', labelsize=15)
 
 st.pyplot(fig1)
 
@@ -147,10 +147,10 @@ sns.barplot(x=labels_d,
             palette='coolwarm',
             ax=ax2)
 
-ax2.set_xlabel("None")
-ax2.set_ylabel("None")
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
+ax2.set_xlabel(None)
+ax2.set_ylabel(None)
+ax2.tick_params(axis='y', labelsize=20)
+ax2.tick_params(axis='x', labelsize=15)
 
 st.pyplot(fig2)
 
@@ -169,10 +169,10 @@ sns.barplot(x=labels_ss,
             palette='coolwarm',
             ax=ax3)
 
-ax3.set_xlabel('None')
-ax3.set_ylabel('None')
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
+ax3.set_xlabel(None)
+ax3.set_ylabel(None)
+ax3.tick_params(axis='y', labelsize=20)
+ax3.tick_params(axis='x', labelsize=15)
 
 st.pyplot(fig3)
 
